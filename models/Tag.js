6aa9842094d10290474d77b63189f6ddaeba0982
JMADA257 <jadams1812@hotmail.com>
2023-10-01 18:51:52 -0600

    Added basic file structure, as well as adding models

---
+++ models/Tag.js
@@ -0,0 +1,28 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection.js");
+
+class Tag extends Model {}
+
+Tag.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "tag",
+  }
+);
+
+module.exports = Tag;
